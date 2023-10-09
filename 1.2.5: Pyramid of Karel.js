cd120bdc9ba13b52d067a725dc8f8620c73e4ae5
AP_Gaming123 <83433323+aditeyapatakoti@users.noreply.github.com>
2023-10-09 11:37:20 -0400

    Update 1.2.5: Pyramid of Karel.js
    made code cleaner

--- 1.2.5: Pyramid of Karel.js
+++ 1.2.5: Pyramid of Karel.js
@@ -7,16 +7,13 @@ for(var i = 0; i < 2; i++) {
     putBall();
     move();
 }
-putBall();
-turnLeft();
-move();
-turnLeft();
-move();
-putBall();
-turnLeft();
-move();
-turnLeft();
-move();
-turnLeft();
-turnLeft();
-turnLeft();
+for(var i = 0; i < 2; i++){
+    putBall();
+    turnLeft();
+    move();
+    turnLeft();
+    move();
+}
+for(var i = 0; i < 3; i++){
+    turnLeft();
+}
