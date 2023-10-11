b7a59ca1dc2c6cde415cd6fa300776e6f89e8eb5
AP_Gaming123 <83433323+aditeyapatakoti@users.noreply.github.com>
2023-10-11 11:32:05 -0400

    fixed overlapping code error
    the original code still works, this just makes it look better + more functional.

--- 4.2.3 Draw Something.js
+++ 4.2.3 Draw Something.js
@@ -1,7 +1,8 @@
 function start(){
 	var line = new Line(1,1,2,2);
 	line.setColor(Color.red);
-	add(line); var circle = new Circle(30);
+	add(line); 
+	var circle = new Circle(30);
 	circle.setPosition(100,50);
 	circle.setColor(Color.blue);
 	add(circle);
